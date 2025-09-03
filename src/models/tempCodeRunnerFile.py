02f598c32f1e34b82e80dcb0a348e257c6fc1d24
Dev-debasish-09 <debasishpra314@gmail.com>
2025-09-03 16:33:18 +0530

    pushing full local project code

---
+++ src/models/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+                        sk_model=model,
+                                artifact_path="uber-demand-prediction",
+                                signature=model_signature,
+                                pip_requirements=["-r requirements.txt"]  # correct way
